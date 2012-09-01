c33175bc1efb1b8af72e68f50739c2f1eeb2cf3e
Matt Young <mpyoung@gmail.com>
2012-09-01 23:10:07 +0100

    Initial commit of duo rest service using c sharp

---
+++ DuoSecurity/RestApiService.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+using HttpWebAdapters;
+
+namespace DuoVerificationService
+{
+    public class RestApiService
+    {
+        private readonly string _integrationKey;
+        private readonly string _secretKey;
+        private readonly string _host;
+
+        public RestApiService(string integrationKey, string secretKey, string host)
+        {
+            _integrationKey = integrationKey;
+            _secretKey = secretKey;
+            _host = host;
+        }
+
+        public Object QueryDuoApi(IHttpWebRequest duoWebRequest)
+        {
+            try
+            {
+                var duoResponse = duoWebRequest.GetResponse();
+
+                if (duoResponse != null)
+                {
+                    var responseStream = duoResponse.GetResponseStream();
+                    if (responseStream != null)
+                    {
+                        using (var reader = new StreamReader(responseStream))
+                        {
+                            var js = new JavaScriptSerializer();
+                            var objText = reader.ReadToEnd();
+                            return js.DeserializeObject(objText);
+                        }
+                    }
+                }
+            }
+            catch (WebException)
+            {
+                return null;
+            }
+            return null;
+        }
+
+        public IHttpWebRequest CreateSignedDuoWebRequest(IHttpWebRequestFactory webRequestFactory, string protocol, string path, HttpWebRequestMethod method, IEnumerable<KeyValuePair<string, string>> queryValues)
+        {
+            var duoWebRequest = CreateDuoWebRequest(webRequestFactory, protocol, path, method);
+
+            var safeQueryString = CreateSafeQueryString(queryValues);
+
+            SignWebRequest(duoWebRequest, method, path, safeQueryString);
+
+            if(method == HttpWebRequestMethod.POST)SetFormValuesForQuery(duoWebRequest, safeQueryString);
+
+            return duoWebRequest;
+        }
+
+        public string GetAuthorizationKey(HttpWebRequestMethod method, string path, string queryString)
+        {
+            var canon = GetCanonRequest(method, _host, path, queryString);
+            var signedCanon = SignHmac(_secretKey, canon);
+
+            var authorization = _integrationKey + ":" + signedCanon;
+            return Convert.ToBase64String(Encoding.ASCII.GetBytes(authorization));
+        }
+
+        private void SignWebRequest(IHttpWebRequest webRequest, HttpWebRequestMethod method, string path, string queryString)
+        {
+            webRequest.Headers.Add("Authorization", string.Format("Basic {0}", GetAuthorizationKey(method, path, queryString)));
+        }
+
+        private IHttpWebRequest CreateDuoWebRequest(IHttpWebRequestFactory webRequestFactory, string protocol, string path,  HttpWebRequestMethod method)
+        {
+            var duoWebRequest = webRequestFactory.Create(new Uri(string.Format(@"{0}://{1}{2}", protocol, _host, path)));
+            duoWebRequest.Method = method;
+            duoWebRequest.Accept = "application/json";
+            duoWebRequest.Timeout = 3 * 60 * 1000;
+            return duoWebRequest;
+        }
+
+        private static void SetFormValuesForQuery(IHttpWebRequest webRequest, string safeQueryString)
+        {
+            if (webRequest == null) throw new ArgumentNullException("webRequest");
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            var stream = webRequest.GetRequestStream();
+            var bodyBytes = new ASCIIEncoding().GetBytes(safeQueryString);
+            stream.Write(bodyBytes, 0, bodyBytes.Length);
+            stream.Flush();
+            stream.Close();
+        }
+
+        /// <summary>
+        /// Create a duo friendly querystring
+        /// </summary>
+        /// <param name="queryStringItems"></param>
+        /// <returns></returns>
+        private static string CreateSafeQueryString(IEnumerable<KeyValuePair<string, string>> queryStringItems)
+        {
+            return string.Join("&", (from queryStringItem in queryStringItems let key = HttpUtility.UrlEncode(queryStringItem.Key, Encoding.UTF8).Replace("+", "%20").Replace("*", "%2A").Replace("%7E", "~") let value = HttpUtility.UrlEncode(queryStringItem.Value, Encoding.UTF8).Replace("+", "%20").Replace("*", "%2A").Replace("%7E", "~") select string.Format("{0}={1}", key, value)).ToArray());
+        }
+
+        /// <summary>
+        /// Sign the content using the key and SHA1
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        private static String SignHmac(String key, String content)
+        {
+            try
+            {
+                var encoding = Encoding.ASCII;
+                var hmacSha1 = new HMACSHA1(encoding.GetBytes(key));
+                return hmacSha1.ComputeHash(GetContentAsStream(content)).Aggregate("", (s, e) => s + String.Format("{0:x2}", e), s => s);
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Return the content as a MemoryStream
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        private static MemoryStream GetContentAsStream(string content)
+        {
+            var contentAsBytes = Encoding.ASCII.GetBytes(content);
+            var contentAsStream = new MemoryStream(contentAsBytes);
+            return contentAsStream;
+        }
+
+        /// <summary>
+        /// Build up the canon required for signing
+        /// </summary>
+        /// <param name="method"></param>
+        /// <param name="host"></param>
+        /// <param name="uri"></param>
+        /// <param name="queryString"></param>
+        /// <returns></returns>
+        private static String GetCanonRequest(HttpWebRequestMethod method, string host, string uri, string queryString)
+        {
+            var canon = new StringBuilder();
+
+            canon.Append(string.Format("{0}\n", method.ToString().ToUpper()));
+            canon.Append(string.Format("{0}\n", host.ToLower()));
+            canon.Append(string.Format("{0}\n", uri));
+            canon.Append(queryString);
+
+            return canon.ToString();
+        }
+
+    }
+}
---
+++ DuoSecurityTests/RestApiServiceTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using DuoVerificationService;
+using HttpWebAdapters;
+using Moq;
+using NUnit.Framework;
+
+namespace DuoSecurityTests
+{
+    [TestFixture]
+    public class RestApiServiceTests
+    {
+        const string IntegrationKey = "1234";
+        const string SecretKey = "abcd";
+        const string Host = "api-xxxxxxxx.duosecurity.com";
+
+
+        [Test]
+        public void AuthorizationKeyShouldBeCorrectlyCalculated()
+        {
+            // Arrange
+            // Act
+            var sut = new RestApiService(IntegrationKey, SecretKey, Host);
+            var result = sut.GetAuthorizationKey(HttpWebRequestMethod.POST, "/rest/v1/auth", "auto=phone1&factor=auto&ipaddr=141.213.231.43&user=bob");
+
+            // Assert
+            Assert.That(result, Is.EqualTo("MTIzNDpmNDYzM2YzZWNhOGQ2YmFmNzk5Mzc5ZDgzMGM3YmU2ODA1Y2FkMWQ2"));
+        }
+
+        [Test]
+        public void CreateSignedDuoWebRequestShouldReturnAWebRequestWhoseHeaderHasHadAuthorizationAddedTo()
+        {
+            // Arrange
+            var queryItems = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("auto", "phone1"),
+                    new KeyValuePair<string, string>("factor", "auto"),
+                    new KeyValuePair<string, string>("ipaddr", "141.213.231.43"),
+                    new KeyValuePair<string, string>("user", "bob")
+                };
+
+            var mockWebRequest = new Mock<IHttpWebRequest>(MockBehavior.Strict);
+            mockWebRequest.SetupSet(o => o.Method = HttpWebRequestMethod.POST).Verifiable("Method not correctly set");
+            mockWebRequest.SetupSet(o => o.Accept = "application/json").Verifiable("Json not set as acceptable");
+            mockWebRequest.SetupSet(o => o.Timeout = 180000).Verifiable("Timeout is not set");
+            mockWebRequest.SetupSet(o => o.ContentType = "application/x-www-form-urlencoded").Verifiable("Form content type is not set");
+            mockWebRequest.Setup(o => o.Headers.Add("Authorization", "Basic MTIzNDpmNDYzM2YzZWNhOGQ2YmFmNzk5Mzc5ZDgzMGM3YmU2ODA1Y2FkMWQ2")).Verifiable("Authorization header not added");
+            mockWebRequest.Setup(o => o.GetRequestStream()).Returns(new MemoryStream()).Verifiable("Request stream is not returned");
+
+            var mockWebRequestFactory = new Mock<IHttpWebRequestFactory>(MockBehavior.Strict);
+            mockWebRequestFactory.Setup(o => o.Create(new Uri("https://api-xxxxxxxx.duosecurity.com/rest/v1/auth"))).Returns(mockWebRequest.Object).Verifiable("Uri was incorrect");
+
+            // Act
+            var sut = new RestApiService(IntegrationKey, SecretKey, Host);
+            sut.CreateSignedDuoWebRequest(mockWebRequestFactory.Object, "https", "/rest/v1/auth", HttpWebRequestMethod.POST, queryItems);
+
+            // Assert
+            mockWebRequestFactory.Verify();
+            mockWebRequest.Verify();
+        }
+
+        [Test]
+        public void QueryDuoApiShouldReturnObjectFromJsonStream()
+        {
+            // Arrange
+            var mockWebResponse = new Mock<IHttpWebResponse>(MockBehavior.Strict);
+            mockWebResponse.Setup(o => o.GetResponseStream()).Returns(new MemoryStream()).Verifiable();
+
+            var mockWebRequest = new Mock<IHttpWebRequest>(MockBehavior.Strict);
+            mockWebRequest.Setup(o => o.GetResponse()).Returns(mockWebResponse.Object);
+
+
+            // Act
+            var sut = new RestApiService(IntegrationKey, SecretKey, Host);
+            var result = sut.QueryDuoApi(mockWebRequest.Object);
+
+            // Assert
+            mockWebRequest.Verify();
+        }
+
+        [Test]
+        public void QueryDuoApiShouldResturnNullIfGetResponseReturnsNull()
+        {
+            // Arrange
+            var mockWebRequest = new Mock<IHttpWebRequest>();
+            mockWebRequest.Setup(o => o.GetResponse()).Returns((IHttpWebResponse)null);
+
+
+            // Act
+            var sut = new RestApiService(IntegrationKey, SecretKey, Host);
+            var result = sut.QueryDuoApi(mockWebRequest.Object);
+
+            // Assert
+            Assert.That(result,Is.Null);
+  
+        }
+
+        [Test]
+        public void QueryDuoApiShouldResturnNullIfGetResponseStreamReturnsNull()
+        {
+            // Arrange
+            var mockWebResponse = new Mock<IHttpWebResponse>();
+            mockWebResponse.Setup(o => o.GetResponseStream()).Returns((Stream)null);
+
+            var mockWebRequest = new Mock<IHttpWebRequest>();
+            mockWebRequest.Setup(o => o.GetResponse()).Returns(mockWebResponse.Object);
+
+            // Act
+            var sut = new RestApiService(IntegrationKey, SecretKey, Host);
+            var result = sut.QueryDuoApi(mockWebRequest.Object);
+
+            // Assert
+            Assert.That(result, Is.Null);
+
+        }
+
+        [Test]
+        public void QueryDuoApiShouldReturnNullIfThereIsAnException()
+        {
+            // Arrange
+            var mockWebRequest = new Mock<IHttpWebRequest>();
+            mockWebRequest.Setup(o => o.GetResponse()).Throws(new WebException());
+
+
+            // Act
+            var sut = new RestApiService(IntegrationKey, SecretKey, Host);
+            var result = sut.QueryDuoApi(mockWebRequest.Object);
+
+            // Assert
+            Assert.That(result, Is.Null);
+  
+        }
+    }
+}
